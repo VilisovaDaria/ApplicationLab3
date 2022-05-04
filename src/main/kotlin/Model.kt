92f6bed6a8cdc8101cfe48a5e13acb87f83d7189
VilisovaDaria <daria.v2906@gmail.com>
2022-05-04 23:28:14 +0300

    +

---
+++ src/main/kotlin/Model.kt
@@ -0,0 +1,22 @@
+import javafx.scene.paint.Color
+import kotlin.math.abs
+
+class Checkers {
+
+    // поле 8Х8, 12 шт. шашек у каждого игрока
+
+
+    private val positionX = Int
+    private val positionY = Int
+
+    fun checkerMovement() {
+        val newPositionX = Int
+        val newPositionY = Int
+
+    }
+
+
+// перемещение шашки, перемещение если дамка
+}
+
+
---
+++ src/main/kotlin/View.kt
@@ -0,0 +1,36 @@
+import javafx.application.Application
+import javafx.scene.Group
+import javafx.scene.Scene
+import javafx.scene.canvas.Canvas
+import javafx.scene.image.Image
+import javafx.stage.Stage
+import java.io.FileInputStream
+
+
+fun main() {
+    Application.launch(MyFirstChart::class.java)
+}
+
+class MyFirstChart: Application() {
+
+    override fun start(stage: Stage) {
+
+        val root = Group()
+        stage.scene = Scene(root)
+        stage.title = "Русские шашки"
+
+        val canvasNew = Canvas(700.0, 700.0)
+        root.children.add(canvasNew)
+
+        val gc = canvasNew.graphicsContext2D
+        val image = Image(FileInputStream("src/main/board.jpg"))
+        gc.drawImage(image, 0.0, 0.0)
+
+        stage.icons.add(Image(FileInputStream("src/main/1.png")))
+        val i = Image(FileInputStream("src/main/1.png"))
+        gc.drawImage(i, 0.0, 0.0)
+
+        stage.show()
+    }
+
+}
