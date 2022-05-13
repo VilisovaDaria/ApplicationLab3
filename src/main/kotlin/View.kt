0155c1e3376cac61617f83b259109df6e931b124
VilisovaDaria <daria.v2906@gmail.com>
2022-05-13 17:13:55 +0300

    +

--- src/main/kotlin/View.kt
+++ src/main/kotlin/View.kt
@@ -6,7 +6,6 @@ import javafx.scene.image.Image
 import javafx.stage.Stage
 import java.io.FileInputStream
 
-
 fun main() {
     Application.launch(MyFirstChart::class.java)
 }
@@ -26,9 +25,41 @@ class MyFirstChart: Application() {
         val image = Image(FileInputStream("src/main/board.jpg"))
         gc.drawImage(image, 0.0, 0.0)
 
-        stage.icons.add(Image(FileInputStream("src/main/1.png")))
+        fun firstArrangement() {
+            val imBlack = Image(FileInputStream("src/main/chessBlack.png"))
+            val imWhite = Image(FileInputStream("src/main/chessWhite.png"))
 
+            getCoordinates()
+            for ((i, b) in getCoordinates()) {
+                if (i < 4.0) {
+                    gc.drawImage(imWhite, b.first, b.second)
+                }
+                if (i > 6.0) {
+                    gc.drawImage(imBlack, b.first, b.second)
+                }
+            }
+        }
+
+        firstArrangement()
         stage.show()
     }
 
+}
+
+fun getCoordinates(): Map<Double, Pair<Double, Double>> {
+    val result = mutableMapOf<Double, Pair<Double, Double>>()
+    val coefficientsFirst = listOf(1 to 1, 2 to 3, 3 to 5, 4 to 7)
+    val coefficientsSecond = listOf(2 to 2, 3 to 4, 4 to 6, 5 to 8)
+
+    for ((m,n) in coefficientsFirst) {
+        for ((i, k) in coefficientsFirst) {
+            result[n + (i.toDouble() / 10.0)] = (70 * k).toDouble() to 70.0 * n
+        }
+    }
+    for ((m,n) in coefficientsSecond) {
+        for ((i, k) in coefficientsSecond) {
+            result[n + ((i-1).toDouble() / 10.0)] = (70 * k).toDouble() to 70.0 * n
+        }
+    }
+    return result.toSortedMap()
 }
