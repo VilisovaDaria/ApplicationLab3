7c886fd9c0630e5245ede8a0c9b08f36a625e1d3
Gloff228 <romo4ka101@gmail.com>
2022-05-14 12:39:54 +0300

    1

--- src/main/kotlin/Chess.kt
+++ src/main/kotlin/Chess.kt
@@ -1,16 +1,30 @@
-class Chess {
+import java.io.FileInputStream
+import javafx.scene.image.Image
+
+enum class Color(private val image: Image) {
+    BLACK(Image(FileInputStream("src/main/chessBlack.png"))),
+    WHITE(Image(FileInputStream("src/main/chessWhite.png")));
+
+    @JvmName("getImage1")
+    fun getImage(): Image {
+        return image
+    }
+}
+
+
+class Chess(var x: Int, var y: Int, color: Color?) {
     // у шашки есть цвет
     private val white = 1
-    private val black = 0
+    private val black = -1
 
     //функция получения ячейки
     fun getCell(): Set<Double> {
-        return getCoordinates().keys
+        return drawChips().keys
     }
 
     //получение координат
     fun getXY(): Collection<Pair<Double, Double>> {
-        return getCoordinates().values
+        return drawChips().values
     }
 
     //функции: если ячейка пустая и соседняя возможен ход;
--- src/main/kotlin/View.kt
+++ src/main/kotlin/View.kt
@@ -10,7 +10,7 @@ fun main() {
     Application.launch(MyFirstChart::class.java)
 }
 
-class MyFirstChart: Application() {
+class MyFirstChart : Application() {
 
     override fun start(stage: Stage) {
 
@@ -25,41 +25,65 @@ class MyFirstChart: Application() {
         val image = Image(FileInputStream("src/main/board.jpg"))
         gc.drawImage(image, 0.0, 0.0)
 
-        fun firstArrangement() {
-            val imBlack = Image(FileInputStream("src/main/chessBlack.png"))
-            val imWhite = Image(FileInputStream("src/main/chessWhite.png"))
 
-            getCoordinates()
-            for ((i, b) in getCoordinates()) {
+
+        fun drawChess() {
+            val blackChess = Color.BLACK.getImage()
+            val whiteChess = Color.WHITE.getImage()
+
+
+            for ((i, b) in drawChips()) {
                 if (i < 4.0) {
-                    gc.drawImage(imWhite, b.first, b.second)
+
+                    gc.drawImage(whiteChess, b.first, b.second)
                 }
                 if (i > 6.0) {
-                    gc.drawImage(imBlack, b.first, b.second)
+                    gc.drawImage(blackChess, b.first, b.second)
                 }
             }
         }
 
-        firstArrangement()
+
+        //Определяю цвет для шашки, если в ячейке нет шашки, то null
+        fun setColor(y: Int): Color? {
+            return when (y) {
+                in 0 until 3 -> Color.WHITE
+                in 5 until 8 -> Color.BLACK
+                else -> null
+            }
+        }
+
+        //заполняю board шашками
+        fun fillBoard(): Array<Array<Chess>> {
+            var board = arrayOf<Array<Chess>>()
+            for (x in 0 until 4) {
+                var array = arrayOf<Chess>()
+                for (y in 0 until 8) {
+                    val a = setColor(y)
+                    array += Chess(x, y, a)
+                }
+                board += array
+            }
+            return board
+        }
+
+        var board = fillBoard()
+        drawChess()
         stage.show()
     }
 
 }
 
-fun getCoordinates(): Map<Double, Pair<Double, Double>> {
+fun drawChips(): Map<Double, Pair<Double, Double>> {
     val result = mutableMapOf<Double, Pair<Double, Double>>()
-    val coefficientsFirst = listOf(1 to 1, 2 to 3, 3 to 5, 4 to 7)
-    val coefficientsSecond = listOf(2 to 2, 3 to 4, 4 to 6, 5 to 8)
+    val coefficient = listOf(1 to 1, 2 to 3, 3 to 5, 4 to 7)
 
-    for ((m,n) in coefficientsFirst) {
-        for ((i, k) in coefficientsFirst) {
+    for ((_, n) in coefficient) {
+        for ((i, k) in coefficient) {
             result[n + (i.toDouble() / 10.0)] = (70 * k).toDouble() to 70.0 * n
+            result[(n + 1) + ((i + 1).toDouble() / 10.0)] = (70 * (k + 1)).toDouble() to 70.0 * (n + 1)
         }
     }
-    for ((m,n) in coefficientsSecond) {
-        for ((i, k) in coefficientsSecond) {
-            result[n + ((i-1).toDouble() / 10.0)] = (70 * k).toDouble() to 70.0 * n
-        }
-    }
+
     return result.toSortedMap()
 }
