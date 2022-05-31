b8281fc809eb478ce3d6c36d5299680c4e66d588
VilisovaDaria <daria.v2906@gmail.com>
2022-05-31 14:51:57 +0300

    .

--- src/main/kotlin/view/View.kt
+++ src/main/kotlin/view/View.kt
@@ -2,7 +2,6 @@ package view
 
 import utils.*
 import controller.Controller
-import controller.notReady
 import javafx.application.Application
 import javafx.event.EventHandler
 import javafx.scene.Group
@@ -11,9 +10,8 @@ import javafx.scene.canvas.Canvas
 import javafx.scene.control.Button
 import javafx.stage.Stage
 import javafx.scene.image.ImageView
-import javafx.scene.layout.AnchorPane
-import tornadofx.add
 import kotlin.system.exitProcess
+
 
 fun main() {
     Application.launch(App::class.java)
@@ -23,49 +21,36 @@ open class App : Application() {
 
     override fun start(stage: Stage) {
         stage.isResizable = false
-
-
-        val viewFirst = ImageView(exit)
-        viewFirst.fitHeight = 35.0
-        viewFirst.isPreserveRatio = true
-        val buttonForExit = Button()
-        buttonForExit.translateX = 620.0
-        buttonForExit.translateY = 10.0
-        buttonForExit.graphic = viewFirst
-
-        val viewSecond = ImageView(restart)
-        viewSecond.fitHeight = 35.0
-        viewSecond.isPreserveRatio = true
-        val buttonForRestart = Button()
-        buttonForRestart.translateX = 10.0
-        buttonForRestart.translateY = 10.0
-        buttonForRestart.graphic = viewSecond
-
-        val root = Group(buttonForRestart, buttonForExit)
+        val root = Group()
         stage.scene = Scene(root)
         stage.title = "Русские шашки"
         val canvasNew = Canvas(700.0, 700.0)
-        val d = AnchorPane(canvasNew)
-        val dfirst = d.add(buttonForExit)
-        val dsecond = d.add(buttonForRestart)
-        root.children.add(d)
+        root.children.add(canvasNew)
+
+        val buttonRestart = Button("", ImageView(restart))
+        buttonRestart.style = "-fx-background-color: transparent;"
+        buttonRestart.layoutX = 6.0
+        buttonRestart.layoutY = 10.0
+
+        val buttonExit = Button("", ImageView(exit))
+        buttonExit.style = "-fx-background-color: transparent;"
+        buttonExit.layoutX = 610.0
+        buttonExit.layoutY = 10.0
+
+
+        root.children.add(buttonRestart)
+        root.children.add(buttonExit)
 
         val gc = canvasNew.graphicsContext2D
         val controller = Controller()
         controller.createBoard()
+
 
         fun repaintScene() {
             gc.drawImage(background, 0.0, 0.0)
             for ((image, x, y) in controller.getSourcesToRepaint()) {
                 gc.drawImage(image, x, y)
             }
-        }
-
-        buttonForExit.onMouseClicked = EventHandler { exitProcess(0) }
-
-        buttonForRestart.onMouseClicked = EventHandler {event ->
-            controller.restart()
-            repaintScene()
         }
 
         stage.scene.onMousePressed =
@@ -79,6 +64,14 @@ open class App : Application() {
                     repaintScene()
 
             }
+
+        buttonRestart.onMouseClicked = EventHandler {
+            controller.restart()
+            repaintScene()
+        }
+
+        buttonExit.onMouseClicked = EventHandler { exitProcess(0) }
+
         repaintScene()
         stage.show()
     }
